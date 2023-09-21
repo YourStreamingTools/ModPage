6851baff4913c30f3d4c8103aa5e24be4ae63ead
GFAUnDead <contact@gfaundead.stream>
2023-09-21 15:42:18 +1000

    Update loggedout.php

--- loggedout.php
+++ loggedout.php
@@ -15,7 +15,7 @@
 <nav class="navbar navbar-default">
     <div class="container-fluid">
         <div class="navbar-header">
-          <a class="navbar-brand" href="https://gfaundead.stream">StreamingTools</a>
+          <a class="navbar-brand" href="">StreamingTools</a>
         </div>
         <ul class="nav navbar-nav">
             <li><a href="login.php">Login</a></li>
