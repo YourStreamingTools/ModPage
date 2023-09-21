50f24937f1a12c179c636e1b2bde9564d9efec31
GFAUnDead <contact@gfaundead.stream>
2023-09-21 15:40:14 +1000

    Create loggedout.php

---
+++ loggedout.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>StreamingTools - Logged Out</title>
+    <link rel="icon" href="https://cdn.yourstreaming.tools/img/logo.png" sizes="32x32" />
+    <link rel="icon" href="https://cdn.yourstreaming.tools/img/logo.png" sizes="192x192" />
+    <link rel="apple-touch-icon" href="https://cdn.yourstreaming.tools/img/logo.png" />
+    <meta name="msapplication-TileImage" content="https://cdn.yourstreaming.tools/img/logo.png" />
+    <link rel="stylesheet" href="include/bootstrap.css">
+    <script src="include/bootstrap.min.js"></script>
+    <script src="include/jquery.min.js"></script>
+    <link rel="stylesheet" href="include/style.css">
+</head>
+<body>
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="https://gfaundead.stream">StreamingTools</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li><a href="login.php">Login</a></li>
+            <li class="active"><a href="loggedout.php">Logged Out</a></li>
+        </ul>
+        <p class="navbar-text navbar-right">&copy; <?php echo date("Y"); ?> StreamingTools. All rights reserved.</p>
+    </div>
+</nav>
+<h1>You have been successfully logged out of the mods dashboard.</h1>
+</body>
+</html>
