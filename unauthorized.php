b51f7b5b6a5ad88e03351838e35ac4264866b4d9
GFAUnDead <contact@gfaundead.stream>
2023-09-21 15:40:33 +1000

    Create unauthorized.php

---
+++ unauthorized.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>StreamingTools - UNAUTHORIZED</title>
+    <link rel="icon" href="https://cdn.yourstreaming.tools/img/logo.png" sizes="32x32" />
+    <link rel="icon" href="https://cdn.yourstreaming.tools/img/logo.png" sizes="192x192" />
+    <link rel="apple-touch-icon" href="https://cdn.yourstreaming.tools/img/logo.png" />
+    <meta name="msapplication-TileImage" content="https://cdn.yourstreaming.tools/img/logo.png" />
+    <link rel="stylesheet" href="include/bootstrap.css">
+    <script src="include/bootstrap.min.js"></script>
+    <script src="include/jquery.min.js"></script>
+    <link rel="stylesheet" href="include/style.css">
+</head>
+<body>
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="">StreamingTools</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li><a href="login.php">Login</a></li>
+        </ul>
+        <p class="navbar-text navbar-right">&copy; <?php echo date("Y"); ?> StreamingTools. All rights reserved.</p>
+    </div>
+</nav>
+<h1 style="text-align: center;">YOU HAVE BEEN DENIED ACCESS TO THE DASHBOARD.</h1>
+<h1 style="text-align: center;">IF YOU BELIEVE THIS IS INCORRECT, CONTACT STREAMINGTOOLS.</h1>
+</body>
+</html>
